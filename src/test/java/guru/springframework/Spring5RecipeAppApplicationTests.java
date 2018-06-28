cd050c4f380b0c7b2c741b5ceaac2796a92d2e55
Rich Krysinski <rich@krysinski.net>
2018-06-28 15:34:28 -0400

    Fix git conflict in file

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -5,11 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-<<<<<<< HEAD
-
-=======
-//@Ignore
->>>>>>> refs/heads/master
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5RecipeAppApplicationTests {
