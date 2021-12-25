42fd4d9a3b2c0b65c4b149216399caa89e0077ba
Mohammadali Azani <aliazn2013@yahoo.com>
2021-12-25 15:44:05 +0330

    Refactor Image service.

--- src/main/java/guru/springframework/service/ImageService.java
+++ src/main/java/guru/springframework/service/ImageService.java
@@ -6,6 +6,5 @@ import org.springframework.web.multipart.MultipartFile;
  * Created by jt on 7/3/17.
  */
 public interface ImageService {
-
     void saveImageFile(String recipeId, MultipartFile file);
 }
--- src/main/java/guru/springframework/service/ImageServiceImpl.java
+++ src/main/java/guru/springframework/service/ImageServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Created by jt on 7/3/17.
@@ -15,37 +16,30 @@ import java.io.IOException;
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
-
-
     private final RecipeRepository recipeRepository;
 
-    public ImageServiceImpl( RecipeRepository recipeService) {
-
+    public ImageServiceImpl(RecipeRepository recipeService) {
         this.recipeRepository = recipeService;
     }
 
     @Override
     @Transactional
     public void saveImageFile(String recipeId, MultipartFile file) {
-
         try {
-            Recipe recipe = recipeRepository.findById(recipeId).get();
-
+            Recipe recipe = recipeRepository.findById(recipeId).orElse(null);
             Byte[] byteObjects = new Byte[file.getBytes().length];
 
             int i = 0;
-
-            for (byte b : file.getBytes()){
+            for (byte b : file.getBytes()) {
                 byteObjects[i++] = b;
             }
 
-            recipe.setImage(byteObjects);
+            Objects.requireNonNull(recipe).setImage(byteObjects);
 
             recipeRepository.save(recipe);
         } catch (IOException e) {
             //todo handle better
             log.error("Error occurred", e);
-
             e.printStackTrace();
         }
     }
