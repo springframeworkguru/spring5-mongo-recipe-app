e905df8d22f4d76cb90eb110960fc04925fae7f5
Simanta.Sarma <ximanta.sarma@gmail.com>
2022-11-04 13:41:10 +0530

    Updates to Spring Boot 2.7.2, Java 17, JUnit 5

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -1,17 +1,13 @@
 package guru.springframework;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class Spring5RecipeAppApplicationTests {
 
-	@Test
+@Test
 	public void contextLoads() {
 	}
 
